290b33b501bf5b3c6cae5ce8c90b7f2ab23dfff6
Arjun <arjunagw02@gmail.com>
2026-04-17 19:41:01 -0600

    completed basic calculator

--- calculator/script.js
+++ calculator/script.js
@@ -8,36 +8,119 @@ function multiply(x, y) { return x * y; }
 function divide(x, y) {return x / y; }
 
 
-function operate(operand1, operator, operand2) {
-    // convert operand strings to numbers
+function execute(operand1, operator, operand2) {
     operand1 = Number(operand1);
     operand2 = Number(operand2);
 
-    // execute appropriate operation
+    let result;
     switch(operator) {
         case "+":
-            return add(operand1, operand2);
+            result = add(operand1, operand2);
+            break;
         case "-":
-            return subtract(operand1, operand2);
-        case "*":
-            return multiply(operand1, operand2);
+            result = subtract(operand1, operand2);
+            break;
+        case "x":
+            result = multiply(operand1, operand2);
+            break;
         case "/":
-            return divide(operand1, operand2);
+            result = divide(operand1, operand2);
+            break;
         default:
             return "UNKNOWN";
     }
+    
+    // Round to 2 decimal places
+    return parseFloat(result.toFixed(8));
 }
 
-// inputs (operands and operators)
-const inputs = [];
 
+function appendOperator(operator, inputs) {
+    if (inputs.length === 0 || inputs.length === 2) {
+        return inputs;
+    } else if (inputs.length === 1) {
+        inputs.push(operator);
+        inputs.push("_");
+    } else if (inputs.length === 3 && inputs.at(2) !== "_"){ // inputs length === 3
+        const calculation = execute(inputs[0], inputs[1], inputs[2]);
+        inputs = [calculation.toString(), operator, "_"];
+    }
+    return inputs;
+}
+
+
+function handleDec(inputs) {
+    // format properly
+    if (inputs.at(-1).includes("_")) {
+        inputs[inputs.length - 1] = "0.";
+    // number already contains decimal
+    } else if (inputs.at(-1).includes(".")) {
+        return inputs;
+    // concat decimal
+    } else {
+        inputs[inputs.length - 1] = inputs[inputs.length - 1] + ".";
+    }
+    return inputs;
+}
+
+
+function concatNum(num, inputs) {
+    // format properly
+    if (inputs.at(-1).includes("_")) {
+        inputs[inputs.length - 1] = "";
+    }
+    // concat number
+    inputs[inputs.length - 1] = inputs[inputs.length - 1] + num;
+    return inputs;
+}
+
+
+function route(target, inputs) {
+    const text = target.textContent;
+    
+    // if number
+    if (/^[0-9]$/.test(text)) {
+        inputs = concatNum(text, inputs);
+    }
+    // if decimal
+    else if (target.classList.contains('dec')) {
+        inputs = handleDec(inputs);
+    }
+    // if operation
+    else if (target.classList.contains('op')) {
+        inputs = appendOperator(text, inputs);
+    }
+    // if clear
+    else if (target.classList.contains('clear')) {
+        inputs = ["_"];
+    }
+    // if equal
+    else if (target.classList.contains('equal')) {
+        if (inputs.length === 3) {
+            const result = execute(inputs[0], inputs[1], inputs[2]);
+            inputs = [result.toString()];
+        }
+    }
+    
+    return inputs;
+}
 
 function main() {
-    if (inputs.length == 3) {
-        result = operate(inputs[0], inputs[1], inputs[2]);
-        // empty array
-        inputs.length = 0;
-        // add result
-        inputs.push(result);
-    }
+    let inputs = ["_"];
+
+    const display = document.querySelector("#screen");
+    display.textContent = "_"
+
+    const buttons = document.querySelectorAll(".btn");
+    buttons.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            inputs = route(e.target, inputs);
+            display.textContent = ""
+            for(let input of inputs) {
+                display.textContent += input + " ";
+            }
+        });
+    });
 }
+
+main();
